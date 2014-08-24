0be34f2f11819cdcb2f8344d60517934d21834a3
Koji Tanaka <kj.tanaka@gmail.com>
2014-08-24 00:20:23 -0400

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: chefdk
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+remote_file "/tmp/chefdk-0.2.0-2.el6.x86_64.rpm" do
+  source "https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-0.2.0-2.el6.x86_64.rpm"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+execute "install_chefdk" do
+  user "root"
+  command "rpm -ivh /tmp/chefdk-0.2.0-2.el6.x86_64.rpm"
+  creates "/opt/chefdk"
+end
